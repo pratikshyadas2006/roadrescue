dc9f58039977a8163c7fbbc2f4ac563f4e15194f
unknown <riyachavan1512@gmail.com>
2026-09-08 22:30:43 +0530

    Update API service

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -2,7 +2,9 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class ApiService {
-  static const String baseUrl = "http://10.116.25.33/rr/road_rescue_api";
+  static const String baseUrl =
+    "http://10.175.184.187/roadrescue/road_rescue_api";
+
 
   // ================= REGISTER =================
   static Future<Map<String, dynamic>> registerUser({
